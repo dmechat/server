c74c4c8a9c80c1c6b83b940bbd28397301fb2001
Sai C <saichaitanya88@gmail.com>
2022-04-15 05:30:35 -0400

    Init 2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import * as csurf from "csurf";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("dmechat")
+    .setDescription("Server for dmechat")
+    .setVersion("0.0.1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  app.use(helmet());
+  app.use(csurf())
+  app.enableCors(); // TODO: Fix this. CORS is enabled for all origins
   await app.listen(3000);
 }
 bootstrap();
